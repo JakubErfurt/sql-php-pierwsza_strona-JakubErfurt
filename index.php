3a1e32e08a120848eb386fccf810fe3db0ffda42
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-11-12 08:40:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Jakub Erfurt nr1</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-JakubErfurt">github</a>
+    <br>
+    <a href="orgPracownicy.php">Organizacja i Pracownicy</a>
+    <a href="funkAgregujaca.php">Funkcje agregujące</a>
+</body>
+</html>
+<?php
