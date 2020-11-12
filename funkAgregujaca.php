42dc4b41a3ea80128302dff815ecc9a38b2fe1e6
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-11-12 09:23:55 +0100

    Create funkAgregujaca.php

---
+++ funkAgregujaca.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Jakub Erfurt nr1</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-JakubErfurt">github</a>
+    <br>
+    <a href="index.php">Pracownicy</a>
+    <a href="praciorgy.php">Organizacja i Pracownicy</a>
+</body>
+</html>
+<?php
+    require_once("connect.php");
+
+    $sql = "SELECT SUM(zarobki) as suma FROM pracownicy, organizacja WHERE id_org = dzial";
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+         echo "<li>ok";
+     } else {
+       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+
+    echo("<h1>Zadanie 1</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>Suma</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['suma']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //-------------------------------------------------------------
+
+?>
