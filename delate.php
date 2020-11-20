dded2867ba99eb1944ada6e1288d1b674457278e
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-11-20 10:17:34 +0100

    Create delate.php

---
+++ delate.php
@@ -0,0 +1,29 @@
+<?php
+echo("jestes w delete.php <br>");
+echo $_POST['id'];
+
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "DELETE FROM Pracownik WHERE id= $_POST['id'];";
+
+//wyświetlamy zapytanie $sql
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
