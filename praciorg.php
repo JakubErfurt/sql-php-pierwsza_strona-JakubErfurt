cada4fccdfd8dcc8438fe00fed43f4af276daa88
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-12-03 08:32:06 +0100

    Update praciorg.php

--- praciorg.php
+++ praciorg.php
@@ -8,10 +8,16 @@
 </head>
 <body>
     <h1>Jakub Erfurt nr1</h1>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-JakubErfurt">github</a>
+    <a href="https://github.com/JakubErfurt/sql-php-pierwsza_strona-JakubErfurt">github</a>
     <br>
-    <a href="index.php">Pracownicy</a>
-    <a href="funkAgregujaca.php">Funkcje agregujące</a>
+    <nav class="nav">
+        <a href="index.php">Pracownicy</a>
+        <a href="funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="orgPracownicy.php">Organizacja i pracownicy</a>
+        <a href="dataczas.php">Data i czas</a>
+        <a href="formularz.html">Formularz</a>
+        <a href="danedobazy.php">Dane do bazy</a>
+    </nav>
 </body>
 </html>
 <?php
