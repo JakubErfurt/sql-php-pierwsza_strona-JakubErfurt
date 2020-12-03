4530d910ab06c8df71b6f763ff5c77644d577d1f
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-12-03 08:23:18 +0100

    Update strona123.php

--- strona123.php
+++ strona123.php
@@ -1,5 +1,27 @@
-		<?php
-	echo("jesteś na stronie123.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1 class="name">Jakub Erfurt nr1</h1>
+    <a href="https://github.com/JakubErfurt/sql-php-pierwsza_strona-JakubErfurt" class="github-link">github</a>
+    <br>
+    <nav class="nav">
+        <a href="index.php">Pracownicy</a>
+        <a href="funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="orgPracownicy.php">Organizacja i pracownicy</a>
+        <a href="dataczas.php">Data i czas</a>
+        <a href="formularz.html">Formularz</a>
+        <a href="danedobazy.php">Dane do bazy</a>
+    </nav><br>
+</body>
+</html>
+<?php
+	echo("jesteś na stronie.php");
 	
 	echo("<ul>");
 	
