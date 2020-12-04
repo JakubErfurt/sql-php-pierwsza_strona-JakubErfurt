22d625fc3585a7a684a3b068cd1b8e6fdc210555
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-12-04 10:08:48 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -41,7 +41,7 @@ $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
 $result = mysqli_query($conn, $sql);
 
 echo("<table border='1'>");
-echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Nazwa dzial</th><th>Dzial</th><th>Data urodzenia</th><th>Usuń</th>");
+echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Dzial</th><th>Nazwa dzial</th><th>Data urodzenia</th><th>Usuń</th>");
     while($row = mysqli_fetch_assoc($result)) {
         echo("<tr>");
         echo('<td>'.$row['id_pracownicy'].'</td>'.'<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['nazwa_dzial'].'</td><td>'.$row['data_urodzenia'].'</td>'.
