c3231c37858701b3de9295c6f99e0b8a2595135a
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-12-11 10:00:26 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -1,1 +1,19 @@
 
+<?php
+    require_once("connect.php");
+
+    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 1</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
