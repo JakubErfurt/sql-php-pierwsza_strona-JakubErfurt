554e4eb767d359607093ab4ba06ea8ad45372a1e
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2020-12-11 10:28:37 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -1,4 +1,26 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Jakub Erfurt nr1</h1>
+    <a href="https://github.com/JakubErfurt/sql-php-pierwsza_strona-JakubErfurt">github</a>
+    <br>
+    <nav class="nav">
+        <a href="index.php">Pracownicy</a>
+        <a href="funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="praciorg.php">Organizacja i pracownicy</a>
+        <a href="dataczas.php">Data i czas</a>
+        <a href="formularz.html">Formularz</a>
+        <a href="danedobazy.php">Dane do bazy</a>
+    </nav>
+</body>
+</html>
 <?php
     require_once("connect.php");
 
@@ -17,3 +39,4 @@
             echo("</tr>");
         };
     echo("</table>");
+?>
