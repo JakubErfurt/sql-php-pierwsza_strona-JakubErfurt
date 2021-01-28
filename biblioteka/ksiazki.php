5853cf97368558c7279fd1950af1d80d9f5e636e
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-01-28 14:08:17 +0100

    Add files via upload

---
+++ biblioteka/ksiazki.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Jakub Erfurt nr1</h1>
+    <a href="https://github.com/JakubErfurt/sql-php-pierwsza_strona-JakubErfurt">github</a>
+    <br>
+    <nav class="nav">
+         <a href="index.php">Pracownicy</a>
+        <a href="funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="praciorg.php">Organizacja i pracownicy</a>
+        <a href="dataczas.php">Data i czas</a>
+        <a href="formularz.html">Formularz</a>
+        <a href="danedobazy.html">Dane do bazy</a>
+	<a href="lista.html">Lista</a>
+	<a href="ksiazki.php">Ksiazki</a>
+    </nav>
+</body>
+</html>
+<?php
+    require_once("connect.php");
+
+    $sql = "SELECT * FROM biblAutor_biblTytul, biblAutor WHERE biblAutor_id=biblAutor.id";
+
+    $result = mysqli_query($conn, $sql);
+
+
+    echo("<table border='1'>");
+    echo("<th>id</th><th>biblAutor_id</th><th>biblTytul_id</th><th>biblWypoz</th><th>id</th><th>autor</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id']."</td><td>".$row['biblAutor_id']."</td><td>".$row['biblTytul_id']."</td><td>".$row['biblWypoz']."</td><td>".$row['id']."</td><td>".$row['autor']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+?>
