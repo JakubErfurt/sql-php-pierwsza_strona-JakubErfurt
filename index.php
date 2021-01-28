5e51838cb9a6bc98a56d137eb770bd855c8f443f
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-01-28 14:25:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     <br>
    <nav class="nav">
         <a href="index.php">Pracownicy</a>
-        <a href="funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
         <a href="pracownicy/praciorg.php">Organizacja i pracownicy</a>
-        <a href="dataczas.php">Data i czas</a>
-        <a href="formularz.html">Formularz</a>
-        <a href="danedobazy.php">Dane do bazy</a>
-       <a href="lista.html">Lista</a>
-       <a href="ksiazki.php">Ksiazki</a>
+        <a href="pracownicy/dataczas.php">Data i czas</a>
+        <a href="pracownicy/formularz.html">Formularz</a>
+        <a href="pracownicy/danedobazy.php">Dane do bazy</a>
+       <a href="biblioteka/lista.html">Lista</a>
+       <a href="biblioteka/ksiazki.php">Ksiazki</a>
     </nav>
 </body>
 </html>
