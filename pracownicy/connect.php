6b557ab9d1d0428179a6ac2527be6eea2ef6ed7d
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-01-28 14:45:37 +0100

    Create connect.php

---
+++ pracownicy/connect.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "remotemysql.com";
+    $username = "OuuQyKjM5n";
+    $password = "Xa5cSuc0AP";
+    $dbname = "OuuQyKjM5n";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " .$conn->connect_error);
+    };
+?>
