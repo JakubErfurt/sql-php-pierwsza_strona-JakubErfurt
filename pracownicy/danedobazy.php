44740f8d0e7e8bf1be5b52879c98ee48d3254532
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-02-04 13:39:32 +0100

    Update danedobazy.php

--- pracownicy/danedobazy.php
+++ pracownicy/danedobazy.php
@@ -12,11 +12,11 @@
     <br>
     <nav class="nav">
         <a href="index.php">Pracownicy</a>
-        <a href="pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
-        <a href="pracownicy/praciorg.php">Organizacja i pracownicy</a>
-        <a href="pracownicy/dataczas.php">Data i czas</a>
-        <a href="pracownicy/formularz.html">Formularz</a>
-        <a href="pracownicy/danedobazy.php">Dane do bazy</a>
+        <a href="../pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="../pracownicy/praciorg.php">Organizacja i pracownicy</a>
+        <a href="../pracownicy/dataczas.php">Data i czas</a>
+        <a href="../pracownicy/formularz.html">Formularz</a>
+        <a href="../pracownicy/danedobazy.php">Dane do bazy</a>
 	
     </nav><br>
     <h2>Dodawanie pracownika</h2>
