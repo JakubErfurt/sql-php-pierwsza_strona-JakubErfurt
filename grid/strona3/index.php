d541d578d6e952385ff3105d36381b5b09eaa68c
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-04-15 11:02:21 +0200

    Update index.php

--- grid/strona3/index.php
+++ grid/strona3/index.php
@@ -1,90 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html leng="en">
+<head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="style3.css">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie-edge" />
     <title>CSS Grid</title>
-  </head>
-  <body>
-    <div class="container">
-      <header>
-      <div class="tabela1">
-      <?php
-    require_once("../../connect.php");
-    $sql = "SELECT * FROM mechanicy";
-    echo("<br>");
-    echo($sql);
-    $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-        } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-     }
-    echo('<table border="1">');
-        echo('<th>id</th><th>mechanik</th>');
-        while($row=mysqli_fetch_assoc($result)){
-            echo('<tr>');
-            echo('<td>'.$row['id'].'</td><td>'.$row['mechanik'].'</td>');
-            echo('</tr>');
-     }
-        echo('</table>');
-?>
+    <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <h1></h1>
+            </header>
+            <nav>
+                
+            </nav>
+            <main>
+                
+            </main>
+            <aside>
+                
+            </aside>
+            <footer>
+                
+            </footer>
+            <footer1>
+            
+            </footer1>
         </div>
-      </header>
-      <nav>
-      2
-      </nav>
-      <main>
-      <?php
-    require_once("../../connect.php");
-    $sql = "SELECT warsztat.id, mechanik, rejestracja FROM mechanicy, warsztat, rejestracje where mechanicy.id=warsztat.mechanik_id and rejestracje.id=warsztat.rejestracja_id";
-    echo("<br>");
-    echo($sql);
-    $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-        } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-     }
-    echo('<table border="1">');
-        echo('<th>id</th><th>mechanik</th><th>Uczeń</th>');
-        while($row=mysqli_fetch_assoc($result)){
-            echo('<tr>');
-            echo('<td>'.$row['id'].'</td><td>'.$row['rejestracja'].'</td><td>'.$row['mechanik'].'</td>');
-            echo('</tr>');
-     }
-        echo('</table>');
-?>
-      </main>
-      <footer>
-      4
-      </footer>
-      <aside>
-      <div class="tabela1">
-      <?php
-    require_once("../../connect.php");
-    $sql = "SELECT * FROM rejestracje";
-    echo("<br>");
-    echo($sql);
-    $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-        } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-     }
-    echo('<table border="1">');
-        echo('<th>Id</th><th>rejestracja</th>');
-        while($row=mysqli_fetch_assoc($result)){
-            echo('<tr>');
-            echo('<td>'.$row['id'].'</td><td>'.$row['rejestracja'].'</td>');
-            echo('</tr>');
-     }
-        echo('</table>');
-?>
-        </div>
-      </aside>
-    </div>
-  </body>
+    </body>
 </html>
