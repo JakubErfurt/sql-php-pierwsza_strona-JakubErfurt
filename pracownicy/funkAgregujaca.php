f8e2e49b9b66ec5f72d11f5a94b530c0a2740349
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-04-19 08:29:43 +0200

    Update funkAgregujaca.php

--- pracownicy/funkAgregujaca.php
+++ pracownicy/funkAgregujaca.php
@@ -11,13 +11,7 @@
     <a href="https://github.com/JakubErfurt/sql-php-pierwsza_strona-JakubErfurt">github</a>
     <br>
     <nav class="nav">
-         <a href="index.php">Pracownicy</a>
-        <a href="../pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
-        <a href="../pracownicy/praciorg.php">Organizacja i pracownicy</a>
-        <a href="../pracownicy/formularz.html">Formularz</a>
-        <a href="../pracownicy/danedobazy.php">Dane do bazy</a>
-	<a href="../biblioteka/lista.php">Lista</a>
-	<a href="../biblioteka/ksiazki.php">Ksiazki</a>
+          <?php include"menu.php" ?>
     </nav>
 </body>
 </html>
