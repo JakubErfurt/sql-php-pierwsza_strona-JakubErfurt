29baabecce717c7475d9e52501d1acae440a1865
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-04-19 08:26:25 +0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+        <a href="index.php">Pracownicy</a>
+        <a href="../pracownicy/funkAgregujaca.php">Funkcje agregujące</a>
+        <a href="../pracownicy/praciorg.php">Organizacja i pracownicy</a>
+        <a href="../pracownicy/formularz.html">Formularz</a>
+        <a href="../pracownicy/danedobazy.php">Dane do bazy</a>
+       <a href="../biblioteka/lista.php">Lista</a>
+       <a href="../biblioteka/ksiazki.php">Ksiazki</a>
+       <a href="../grid/index.html">Grid</a>
