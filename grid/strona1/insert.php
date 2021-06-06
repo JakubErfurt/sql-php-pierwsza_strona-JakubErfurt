40144b323c269d25401e29bb668ddcdc09442371
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-06-06 22:04:24 +0200

    Update insert.php

--- grid/strona1/insert.php
+++ grid/strona1/insert.php
@@ -1,7 +1,16 @@
 <?php
+$id_pracownicy=$_POST['id_pracownicy'];
+$imie=$_POST['imie'];
+$dzial=$_POST['dzial'];
+$zarobki=$_POST['zarobki'];
+
 require_once("../../connect.php");
+
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki) 
      VALUES (null,'" .$_POST['imie']."', ".$_POST['dzial'].", ".$_POST['zarobki']."')";
 $result=mysqli_query($conn, $sql);
+
+if($result) echo "Rekord został dodany poprawnie";
+    else echo "Błąd nie udało się dodać nowego rekordu";
 
 ?>
