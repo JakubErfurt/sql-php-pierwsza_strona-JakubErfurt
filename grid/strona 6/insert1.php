fd264a30123561618e4a33ee9eda67464af4e0fb
JakubErfurt <74046043+JakubErfurt@users.noreply.github.com>
2021-06-06 12:02:20 +0200

    Create insert1.php

---
+++ grid/strona 6/insert1.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../../connect.php");
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki)
+VALUES (null, ".$_POST['imie'].",".$_POST['dzial'].",".$_POST['zarobki'].")";
+$result = mysqli_query($conn, $sql);
+?>
